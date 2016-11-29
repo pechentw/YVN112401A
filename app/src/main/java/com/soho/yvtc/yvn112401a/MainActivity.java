1b5455d823d97bc3c1b7b9a07ea60ef2ea3a3a3b
pechentw <pechen.tw@gmail.com>
2016-11-29 20:35:15 +0800

    3rd

--- app/src/main/java/com/soho/yvtc/yvn112401a/MainActivity.java
+++ app/src/main/java/com/soho/yvtc/yvn112401a/MainActivity.java
@@ -4,6 +4,8 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -14,7 +16,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity
+    implements LocationListener {
     // 定位管理器
     private LocationManager mLocationManager;
     // 定位監聽器
@@ -44,27 +51,43 @@ public class MainActivity extends AppCompatActivity {
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
-        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0,
-                new LocationListener() {
-                    @Override
-                    public void onLocationChanged(Location location) {
-                        Log.d("LOC", "Change!!");
-                    }
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+    }
 
-                    @Override
-                    public void onStatusChanged(String provider, int status, Bundle extras) {
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d("LOC", "Change!!");
+        Log.d("LOC", "" + location.getLatitude() + "," + location.getLongitude());
+        Location loc2 = new Location("MyLOC");
+        loc2.setLatitude(24.9532);
+        loc2.setLongitude(121.2261);
 
-                    }
+        double dis = location.distanceTo(loc2);
+        Log.d("LOC", "離中壢火車站:" + dis + "公尺");
 
-                    @Override
-                    public void onProviderEnabled(String provider) {
+        Geocoder gc = new Geocoder(MainActivity.this, Locale.TRADITIONAL_CHINESE);
+        List<Address> lstAddress = null;
+        try {
+            lstAddress = gc.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String returnAddress=lstAddress.get(0).getAddressLine(0);
+        Log.d("LOC", returnAddress);
+    }
 
-                    }
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
 
-                    @Override
-                    public void onProviderDisabled(String provider) {
+    }
 
-                    }
-                });
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
